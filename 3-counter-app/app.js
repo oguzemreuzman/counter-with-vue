5eed221c863f5fdf21304f288715111ca1999540
oguzemreuzman <77250575+oguzemreuzman@users.noreply.github.com>
2022-12-15 19:11:30 +0300

    Add files via upload

---
+++ 3-counter-app/app.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            counter: 0,
+        };
+    },
+    methods : {
+        inc(){
+            this.counter++;
+        },
+        dec(){
+            this.counter--;
+        },
+    },
+}).mount("#app")
